5e8495e969ad6394d64ff6b5c9388bdb11a348b2
Anthony Lynch <anthonyvlynch5@gmail.com>
2021-11-09 17:48:36 -0500

    verifyUser.jsx - on page load, dispatch randomstring from params to saga

--- src/components/VerifyUser/VerifyUser.jsx
+++ src/components/VerifyUser/VerifyUser.jsx
@@ -1,7 +1,18 @@
 import { useParams } from "react-router";
+import { useDispatch } from "react-redux";
+import { useEffect } from "react";
 
 function VerifyUser() {
     const {randomString} = useParams();
+    const dispatch = useDispatch();
+
+    const putVerifyUser = () => {
+        dispatch({type: 'PUT_VERIFY_USER', payload: randomString});
+    }
+
+    useEffect(() => {
+        putVerifyUser();
+    }, []);
 
     return(
         <>
