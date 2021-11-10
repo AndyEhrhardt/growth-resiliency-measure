7704a720e1e791cbd1ff1ad8beb6bd4046080971
anwar <anwarhussein2353@gmail.com>
2021-11-10 16:06:45 -0600

    Added a form to add school and district

--- src/components/SchoolAndDistrict/SchoolAndDistrict.jsx
+++ src/components/SchoolAndDistrict/SchoolAndDistrict.jsx
@@ -1,13 +1,15 @@
 import { useSelector, useDispatch } from "react-redux";
-import { useEffect } from 'react'
+import { useEffect,useState } from 'react'
 import TableContainer from '@material-ui/core/TableContainer';
+import {FormControl,Select} from '@mui/material';
 // import { ArrowBackIcon } from '@mui/icons-material';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
-import{Paper,Button} from '@material-ui/core';
+import TextField from "@mui/material/TextField";
+import { Paper, Button } from '@material-ui/core';
 
 
 function SchoolAndDistrict() {
@@ -15,42 +17,85 @@ function SchoolAndDistrict() {
   const store = useSelector(store => store.districtSchool);
   console.log(store);
   const dispatch = useDispatch();
-  
+
   useEffect(() => {
     dispatch({ type: 'FETCH_DISTRICT_SCHOOL' });
   }, [dispatch])
 
+  const [school, setSchool] = useState('');
+  const [district, setDistrict] = useState('');
+  const [openAdd, setOpenAdd] = useState(false);
+
+  const handleAdd = () => {
+    setOpenAdd(!openAdd);
+
+  }
+
 
   return (
     <>
-    <Paper elavation= {12} sx ={{width: '100%'}}>
-      <Button variant="contained" color="secondary">Student List</Button>
-     
-      <TableContainer>
-      <Table>
-        <TableHead >
-          <TableRow style={{ top: 57, minWidth: 1700 }}>
-            <TableCell>School</TableCell>
-            <TableCell>District</TableCell>
-            <TableCell>Q1</TableCell>
-            <TableCell>Q2</TableCell>
-            <TableCell>Q3</TableCell>
-            <TableCell>Q4</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody className="table_body">
-          <TableRow className="row_data">
-          {store.map((school) => {
-            return <TableCell className="school" key={school.school_name}>{school.school_name} {school.district_name} {school.q1} {school.q2} {school.q3} {school.q4}</TableCell>
-          })}
-          </TableRow>
-        </TableBody>
-      </Table>
-      </TableContainer>
-     
-    </Paper>
-     <Button variant="contained" color="primary">Add School</Button>
-     </>
+      <Paper elavation={12} sx={{ width: '100%' }}>
+        <Button variant="contained" color="secondary">Student List</Button>
+
+        <TableContainer>
+          <Table>
+            <TableHead >
+              <TableRow style={{ top: 57, minWidth: 1700 }}>
+                <TableCell>School</TableCell>
+                <TableCell>District</TableCell>
+                <TableCell>Q1</TableCell>
+                <TableCell>Q2</TableCell>
+                <TableCell>Q3</TableCell>
+                <TableCell>Q4</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody className="table_body">
+              <TableRow className="row_data">
+                {store.map((school) => {
+                  return <TableCell className="school" key={school.school_name}>{school.school_name} {school.district_name} {school.q1} {school.q2} {school.q3} {school.q4}</TableCell>
+                })}
+              </TableRow>
+            </TableBody>
+          </Table>
+        </TableContainer>
+
+      </Paper>
+      <Button onClick={handleAdd} variant="contained" color="primary">Add School</Button>
+
+      <form className="form_add" onSubmit={handleSubmitAdd} >
+        <div className="school_add">
+          <TextField type="text"
+            placeholder="School"
+            value={school}
+            onChange={(event) => setAddSchool(event.target.value)}
+
+          />
+        </div>
+        <div className="district_add">
+              
+        <FormControl>
+          <Select
+          value={district}
+          label="District"
+          onChange={(event) =>setDistrict(event.target.value)}
+          >
+          </Select>
+        </FormControl>
+
+        </div>
+
+        <Button
+        
+        >
+          Add
+
+        </Button>
+        
+
+
+      </form>
+
+    </>
   );
 }
 
