62c7e6e3e00bfa3c310568eaccfb5ac433400163
Anthony Lynch <anthonyvlynch5@gmail.com>
2021-11-11 16:15:34 -0500

    mapped over each question in AssessmentFormPage

--- src/components/AssessmentFormPage/AssessmentFormPage.jsx
+++ src/components/AssessmentFormPage/AssessmentFormPage.jsx
@@ -1,10 +1,30 @@
-import React from 'react'
+import {React, useState} from 'react';
+import AssessmentRadioChild from '../Assessment_Radio_Child/Assessment_Radio_Child';
 
 function AssessmentFormPage() {
+    const [assessmentQ, setAssessmentQ] = useState([
+        {question: 'How would you rate the student’s self-confidence with adults at the beginning of the semester/quarter?', score: ''},
+        {question: 'How would you rate the student’s self-confidence with adults now?', score: ''},
+        {question: 'How would you rate the student’s self-confidence amongst their peers at the beginning of the semester/quarter?', score: ''},
+        {question: 'How would you rate the student’s self-confidence amongst their peers now?', score: ''},
+        {question: 'How would you rate the student’s ability to succeed under pressure at the beginning of the semester/quarter?', score: ''},
+        {question: 'How would you rate the student’s ability to succeed under pressure now?', score: ''},
+        {question: 'How would you rate the student’s ability to make it through adversity at the beginning of the semester/quarter?', score: ''},
+        {question: 'How would you rate the student’s ability to make it through adversity now?', score: ''},
+        {question: 'How would you rate the student’s ability to express who they are as a person with adults at the beginning of the semester/quarter?', score: ''},
+        {question: 'How would you rate the student’s ability to express who they are as a person with adults now?', score: ''},
+        {question: 'How would you rate the student’s ability to express who they are as a person with peers at the beginning of the semester/quarter?', score: ''},
+        {question: 'How would you rate the student’s ability to express who they are as a person with peers now?', score: ''},
+        {question: 'How would you rate the student’s ability to ask for help and get their needs met at the beginning of the semester/quarter?', score: ''},
+        {question: 'How would you rate the student’s ability to ask for help and get their needs met now?', score: ''},
+    ]);
+
     return (
-        <div>
-            
-        </div>
+        <>
+            {assessmentQ.map((question, i) => {
+                <AssessmentRadioChild key={i} question={question} setAssessmentQ={setAssessmentQ}/>
+            })}  
+        </>
     )
 }
 
