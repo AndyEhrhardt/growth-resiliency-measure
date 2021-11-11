96c92418c8b72a3605d4e14784a816b3d8ebcb93
Sarah Fuoss <38589057+SarahTech-alt@users.noreply.github.com>
2021-11-11 14:15:13 -0600

    Added saga functions to filter by quarter and time range. Put descriptions in the overview charts component on formatting data to send.

--- src/components/OverviewCharts/OverviewCharts.jsx
+++ src/components/OverviewCharts/OverviewCharts.jsx
@@ -1,7 +1,28 @@
 function OverviewCharts() {
+    // for parameter only 
+    // dispatch type 'FETCH_ONE_PARAMETER_RESULTS'
+    // payload: parameter
+    // send in format
+    // { race : "name" } || { gender: "name" } || 
+    // { demographics: "hispanic_latino" } ||
+    // { school: "name" } || { district: "name" }
+    // these are examples, should be able to
+    // use any desired parameter in this format
 
-    // "race"."name", "gender"."name", "demographics"."hispanic_latino",
-    // "school"."name", "district"."name"
+    // for search of quarter send selected parameter
+    // dispatch type FETCH_PARAMETER_QUARTER
+    // pyaload: parameter, quarter
+    // formatted as above and the quarter 
+    // should be sent in format of 
+    // { quarter : 1 } || { quarter : 2 }
+    // { quarter : 3 } || { quarter : 4 }
+
+    // for search of time range send selected parameter
+    // dispatch type FETCH_PARAMETER_RANGE
+    // payload: parameter, quarter
+    // formatted as above and the time range
+    // should be sent in format of 
+    // [{start: "2021-09-10"}, {end: "2021"-12-15"}]
     
     return(
         <>
--- src/redux/sagas/overviewcharts.saga.js
+++ src/redux/sagas/overviewcharts.saga.js
@@ -2,23 +2,32 @@ import axios from 'axios';
 import { put, takeLatest } from 'redux-saga/effects';
 
 
-function* getOverview() {
-  console.log("in get charts")
+function* getFilteredByTypeRange(action) {
+  const filterByParameter = action.payload.parameter;
+  const filterByRangeStart = action.payload.start;
+  const filterByRangeEnd = action.payload.end;
+  const filteredByRange = axios.get(`/api/overviewcharts/type/${filterByParameter}/${filterByRangeStart}/${filterByRangeEnd}`);
+  yield put({type: 'SET_OVERVIEW', payload: filteredByRange.data});
+}
 
-
-
-
+function* getFilteredByTypeQuarter(action) {
+  const filterByParameter = action.payload.parameter;
+  const filterTargetQuarter = action.payload.quarter;
+  const filteredByQuarter = axios.get(`/api/overviewcharts/type/${filterByParameter}/${filterTargetQuarter}`);
+  yield put({type: 'SET_OVERVIEW', payload: filteredByQuarter.data});
 }
 
 function* getFilteredByType(action) {
   const filterBy = action.payload;
-  const filteredDemographicData = axios.get(`/api/overviewcharts/type/${filterBy}`);
-  yield put({type: 'SET_OVERVIEW', payload: filteredDemographicData.data});
+  const filteredData = axios.get(`/api/overviewcharts/type/${filterBy}`);
+  yield put({type: 'SET_OVERVIEW', payload: filteredData.data});
 }
 
 function* overviewSaga() {
   yield takeLatest('FETCH_OVERVIEW', getOverview);
-  yield takeLatest('FETCH_DEMOGRAPHIC_RESULTS', getFilteredByType);
+  yield takeLatest('FETCH_PARAMETER_RESULTS', getFilteredByType);
+  yield takeLatest('FETCH_PARAMETER_QUARTER', getFilteredByTypeQuarter),
+  yield takeLatest('FETCH_PARAMETER_RANGE', getFilteredByTypeRange)
 }
 
 export default overviewSaga;
