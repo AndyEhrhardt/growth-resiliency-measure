656d17f280941e9f8acf342cc17f0645ef0dd2ff
Anthony Lynch <anthonyvlynch5@gmail.com>
2021-11-11 15:00:57 -0500

    added radio button form with options 1 to 5 to Assessment_Radio_Child

--- src/components/Assessment_Radio_Child/Assessment_Radio_Child.jsx
+++ src/components/Assessment_Radio_Child/Assessment_Radio_Child.jsx
@@ -1,6 +1,22 @@
+import {Radio, RadioGroup, FormControlLabel} from '@material-ui/core';
+import { useState } from 'react';
+
 function AssessmentRadioChild() {
+    const [score, setScore] = useState('');
+
+    const handleValueChange = (event) => {
+        setScore(event.target.value);
+    }
+
     return(
         <>
+            <RadioGroup row aria-label="Question" value={value} onChange={handleValueChange}>
+                <FormControlLabel value="1" control={<Radio />} label="1" labelPlacement="top"/>
+                <FormControlLabel value="2" control={<Radio />} label="2" labelPlacement="top"/>
+                <FormControlLabel value="3" control={<Radio />} label="3" labelPlacement="top"/>
+                <FormControlLabel value="4" control={<Radio />} label="4" labelPlacement="top"/>
+                <FormControlLabel value="5" control={<Radio />} label="5" labelPlacement="top"/>
+            </RadioGroup>
         </>
     );
 }
