3d8c88ab028baa1b3fd2bfb3e65876d52aa3ad17
Sarah Fuoss <38589057+SarahTech-alt@users.noreply.github.com>
2021-11-15 13:37:13 -0600

    Fixed second dropdown selection.

--- src/components/OverviewCharts/OverviewCharts.jsx
+++ src/components/OverviewCharts/OverviewCharts.jsx
@@ -6,6 +6,8 @@ import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
+import Button from '@mui/material/Button';
+import Stack from '@mui/material/Stack';
 
 
 
@@ -39,15 +41,19 @@ function OverviewCharts() {
     const schoolInfo = useSelector(store => store.districtSchool);
     const demographics = useSelector(store => store.demographics);
 
+    const fetchInfo = () => {
+        dispatch({ type: 'FETCH_PARAMETER_RESULTS', payload: { filterBy: filterValue, searchOn: 'name' } });
+    }
+
     useEffect(() => {
-        dispatch({ type: 'FETCH_PARAMETER_RESULTS', payload: { filterBy: "gender", searchOn: "name" } });
-        dispatch({type: 'FETCH_DISTRICT_SCHOOL'});
-        dispatch({type: 'FETCH_DEMOGRAPHICS'});
+        
+        dispatch({ type: 'FETCH_DISTRICT_SCHOOL' });
+        dispatch({ type: 'FETCH_DEMOGRAPHICS' });
         populateSelections();
     }, [dispatch]);
 
-    const [filterBy, setFilterBy] = useState('');
-    const [searchOn, setSearchOn] = useState('');
+    const [filterValue, setFilterValue] = useState('');
+    const [searchBy, setSearchBy] = useState('');
     const [startTime, endStartTime] = useState('');
     const [selection, setSelection] = useState('');
 
@@ -63,17 +69,17 @@ function OverviewCharts() {
     return (
         <div>
             {/* {JSON.stringify(schoolInfo)} */}
-            {JSON.stringify(demographics)}
+            {/* {JSON.stringify(demographics)} */}
             <Box sx={{ minWidth: 120 }}>
                 <FormControl sx={{ m: 1, minWidth: 120 }}>
                     <InputLabel id="demo-simple-select-label">Filter By:</InputLabel>
                     <Select
+                    defaultValue = ""
                         labelId="demo-simple-select-label"
                         id="demo-simple-select"
-                        value={filterBy}
                         label="Filter By"
-                        onChange={event => setSelection(event.target.value)}
-                        width= '50%'
+                        onChange={event => setFilterValue(event.target.value)}
+                        width='50%'
                     >
                         <MenuItem value={'school'}>School</MenuItem>
                         <MenuItem value={'district'}>District</MenuItem>
@@ -81,66 +87,85 @@ function OverviewCharts() {
                         <MenuItem value={'gender'}>Gender</MenuItem>
                     </Select>
                 </FormControl>
-                {selection == 'school' ?
-                <FormControl sx={{ m: 1, minWidth: 120 }}>
-                <InputLabel id="demo-simple-select-label">Filter By:</InputLabel>
-                <Select
-                    labelId="demo-simple-select-label"
-                    id="demo-simple-select"
-                    value={filterBy}
-                    label="Filter By"
-                    onChange={event => setSearchOn(event.target.value)}
-                    width= '50%'
-                >
-                    <MenuItem>All</MenuItem>
-                    {schoolInfo.map((logs) => (
-                     
-                    <MenuItem value={'name'}>{logs.school_name}</MenuItem>
-                  
-                    ))}
-                </Select>
-            </FormControl> : <></>
+                {filterValue == 'school' ?
+                    <FormControl sx={{ m: 1, minWidth: 120 }}>
+                        <InputLabel id="demo-simple-select-label">Filter By:</InputLabel>
+                        <Select
+                            labelId="demo-simple-select-label"
+                            id="demo-simple-select"
+                            label="Filter By"
+                            onChange={event => setSearchBy(event.target.value)}
+                            width='50%'
+                        >
+                            <MenuItem>All</MenuItem>
+                            {schoolInfo.map((logs) => (
+
+                                <MenuItem value={logs.school_name}>{logs.school_name}</MenuItem>
+
+                            ))}
+                        </Select>
+                    </FormControl> : <></>
                 }
-                 {selection == 'gender' ?
-                <FormControl sx={{ m: 1, minWidth: 120 }}>
-                <InputLabel id="demo-simple-select-label">Filter By:</InputLabel>
-                <Select
-                    labelId="demo-simple-select-label"
-                    id="demo-simple-select"
-                    value={filterBy}
-                    label="Filter By"
-                    onChange={event => setSearchOn(event.target.value)}
-                    width= '50%'
-                >
-                    <MenuItem>All</MenuItem>
-                    {demographics.gender.map((logs) => (
-                     
-                    <MenuItem value={'name'}>{logs.name}</MenuItem>
-                  
-                    ))}
-                </Select>
-            </FormControl> : <></>
+                {filterValue == 'gender' ?
+                    <FormControl sx={{ m: 1, minWidth: 120 }}>
+                        <InputLabel id="demo-simple-select-label">Filter By:</InputLabel>
+                        <Select
+                            labelId="demo-simple-select-label"
+                            id="demo-simple-select"
+                            label="Filter By"
+                            onChange={event => setSearchBy(event.target.value)}
+                            width='50%'
+                        >
+                            <MenuItem>All</MenuItem>
+                            {demographics.gender.map((logs) => (
+
+                                <MenuItem value={logs.name}>{logs.name}</MenuItem>
+
+                            ))}
+                        </Select>
+                    </FormControl> : <></>
                 }
-                   {selection == 'race' ?
-                <FormControl sx={{ m: 1, minWidth: 120 }}>
-                <InputLabel id="demo-simple-select-label">Filter By:</InputLabel>
-                <Select
-                    labelId="demo-simple-select-label"
-                    id="demo-simple-select"
-                    value={filterBy}
-                    label="Filter By"
-                    onChange={event => setSearchOn(event.target.value)}
-                    width= '50%'
-                >
-                    <MenuItem>All</MenuItem>
-                    {demographics.race.map((logs) => (
-                     
-                    <MenuItem value={'name'}>{logs.name}</MenuItem>
-                  
-                    ))}
-                </Select>
-            </FormControl> : <></>
+                {filterValue == 'race' ?
+                    <FormControl sx={{ m: 1, minWidth: 120 }}>
+                        <InputLabel id="demo-simple-select-label">Filter By:</InputLabel>
+                        <Select
+                            labelId="demo-simple-select-label"
+                            id="demo-simple-select"
+                            label="Filter By"
+                            onChange={event => setSearchBy(event.target.value)}
+                            width='50%'
+                        >
+                            <MenuItem>All</MenuItem>
+                            {demographics.race.map((logs) => (
+
+                                <MenuItem value={logs.name}>{logs.name}</MenuItem>
+
+                            ))}
+                        </Select>
+                    </FormControl> : <></>
                 }
+                {filterValue == 'district' ?
+                    <FormControl sx={{ m: 1, minWidth: 120 }}>
+                        <InputLabel id="demo-simple-select-label">Filter By:</InputLabel>
+                        <Select
+                            labelId="demo-simple-select-label"
+                            id="demo-simple-select"
+                            label="Filter By"
+                            onChange={event => setSearchBy(event.target.value)}
+                            width='50%'
+                        >
+                            <MenuItem>All</MenuItem>
+                            {schoolInfo.map((logs) => (
+                                
+                                <MenuItem value={logs.district_name}>{logs.district_name}</MenuItem>
+
+                            ))}
+                        </Select>
+                    </FormControl> : <></>
+                }
+                <Button type="submit" variant="outlined" onClick={fetchInfo} sx={{ m: 1, minWidth: 120, height: 30, mt:2 }} >
+                    Submit
+                </Button>
             </Box>
 
 
